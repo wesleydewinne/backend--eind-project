213b387fee7b299a58a2d5cf4fbc9a6fcaeaa892
wesleydewinne <wesleydewinne@gmail.com>
2025-06-29 10:58:11 +0200

    Eerste commit

---
+++ src/main/java/nl/novi/backend_eindopdracht/BackEndEindopdrachtApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backend_eindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndEindopdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndEindopdrachtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/backend_eindopdracht/BackEndEindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backend_eindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEindopdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
